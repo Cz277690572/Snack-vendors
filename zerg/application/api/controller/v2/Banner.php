480dd6fcc403c34ad9b282048943e194063be082
Cz277690572 <277690572@qq.com>
2017-10-09 19:43:47 +0800

    添加项目文件
    添加项目文件

---
+++ zerg/application/api/controller/v2/Banner.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\api\controller\v2;
+
+use app\api\validate\IDMustBePostiveInt;
+use app\api\model\Banner as BannerModel;
+use app\lib\exception\BannerMissException;
+use think\Exception;
+class Banner
+{	
+	/**
+	 * 获取指定id的banner信息
+	 * @url /banner/:id
+	 * @http GET
+	 * @id banner的id号
+	 */
+	public function getBanner($id)
+	{
+		return 'This is v2!';
+	}
+}
---
+++ zerg/application/extra/secure.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'token_salt' => 'SADFGHJKL',
+	// 微信支付成功的回调地址
+	'pay_back_url' => 'http://z.cn/api/v1/pay/notify'
+];
